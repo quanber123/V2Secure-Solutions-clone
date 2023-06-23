6b905e11b8e21bf00f7e9c1524cb8e9fdd2afeb8
quanber123 <quanber123@icloud.com>
2023-06-24 01:07:17 +0700

    service-detail

--- my-project/src/pages/services/nav-services.jsx
+++ my-project/src/pages/services/nav-services.jsx
@@ -1,40 +1,31 @@
 import { NavLink } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { ServicesContext } from './hooks/services-context';
 import { useTranslation } from 'react-i18next';
 function NavServices() {
   const { t } = useTranslation();
   const { dataServices } = useContext(ServicesContext);
-  const [show, setShow] = useState(false);
-  const handleClickTab = () => {
-    setShow((prevShow) => !prevShow);
-  };
   return (
-    <nav className='container mx-auto my-8 Up'>
-      <button
-        className='px-4 py-3 text-boldBlue border border-blue rounded-md mb-4 '
-        onClick={handleClickTab}
-      >
-        {t('SERVICES')}
-      </button>
-      {show && (
-        <div className='nav-services w-full md:w-1/4 bg-lightGray my-8 rounded-xl p-4 flex flex-col justify-between'>
-          {dataServices.map((service, index) => {
-            return (
-              <NavLink
-                className={({ isActive }) =>
-                  isActive ? 'my-2 text-blue font-bold' : 'my-2'
-                }
-                key={index}
-                to={`../${service.link}`}
-                onClick={handleClickTab}
-              >
-                {service.intro}
-              </NavLink>
-            );
-          })}
-        </div>
-      )}
+    <nav>
+      <h2 className='text-[32px] font-bold Scale'>{t('our-services')}</h2>
+      <div className='nav-services my-8'>
+        {dataServices.map((service, index) => {
+          return (
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? 'nav-services-active text-[20px] leading-[23.48px]'
+                  : 'text-[20px] leading-[23.48px]'
+              }
+              key={index}
+              to={`../${service.link}`}
+            >
+              <h5 className='font-bold'>{service.intro}</h5>
+              <i className='fa fa-angle-right font-bold services'></i>
+            </NavLink>
+          );
+        })}
+      </div>
     </nav>
   );
 }
